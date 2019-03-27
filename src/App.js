c6f57e1a17013abc4fcff5029d0c78b372d491eb
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-03-27 12:53:10 +0300

    [Feature] Create and view notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import Amplify from 'aws-amplify';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import aws_exports from './aws-exports';
+import { createNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
 
 Amplify.configure(aws_exports);
 
 const App = () => {
-  return <div>Hello</div>;
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState('');
+
+  useEffect(() => {
+    handleListNotes();
+  }, []);
+
+  const handleListNotes = async () => {
+    const { data } = await API.graphql(graphqlOperation(listNotes));
+    console.log(data);
+    setNotes(data.listNotes.items);
+  };
+
+  const handleAddNote = async event => {
+    event.preventDefault();
+    const payload = { note };
+    const { data } = await API.graphql(
+      graphqlOperation(createNote, { input: payload })
+    );
+    const newNote = data.createNote;
+    const updatedNotes = [newNote, ...notes];
+    setNotes(updatedNotes);
+    setNote('');
+  };
+
+  return (
+    <div className="flex flex-column items-center justify-center bg-washed-red pa3">
+      <h1 className="code f2">Amplify Notetaker</h1>
+      <form className="mb3" onSubmit={handleAddNote}>
+        <input
+          className="pa2 f4"
+          placeholder="Write your Note"
+          value={note}
+          onChange={({ target }) => setNote(target.value)}
+        />
+        <button className="pa2 f4">Add</button>
+      </form>
+      <div>
+        {notes.map((item, i) => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default withAuthenticator(App, { includeGreetings: true });
