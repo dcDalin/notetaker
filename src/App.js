d1dc4aae08389a5d1b3b8368294346905d53c7b3
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-03-27 10:44:37 +0300

    [Feature] Setup signup and login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import React from 'react';
+import aws_exports from './aws-exports';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+Amplify.configure(aws_exports);
 
-export default App;
+const App = () => {
+  return <div>Hello</div>;
+};
+
+export default withAuthenticator(App, { includeGreetings: true });
